11e989f8bb8edfd9b87abd3be0a88867fd0a73bd
homm <homm86@gmail.com>
2010-11-29 13:07:45 +0500

    getter for timezones is complete

--- timezones/fields.py
+++ timezones/fields.py
@@ -2,11 +2,12 @@
 
 from django.conf import settings
 from django.db import models
-from django.db.models import signals
+from django.db.models import signals, fields
 from django.utils.encoding import smart_unicode, smart_str
+from django.utils.functional import curry
 
 import pytz
-from datetime import datetime
+from datetime import datetime, tzinfo
 
 from timezones import forms, zones
 from timezones.utils import coerce_timezone_value, validate_timezone_max_length
@@ -72,9 +73,6 @@ class TestDateTimeDescriptor(object):
     def __init__(self, field):
         self.field = field
     
-    def get_timezone(self, instance):
-        return pytz.timezone('Asia/Yekaterinburg')
-    
     def __set__(self, instance, value):
         """
         Метод __set__ вызывается только когда полю назаначаются данные из вне. А все данные из вне приходят 
@@ -85,14 +83,15 @@ class TestDateTimeDescriptor(object):
         cache_name = self.field.get_cache_name()
         if cache_name in instance.__dict__:
             del instance.__dict__[cache_name]
-        del instance.__dict__[self.field.attname]
+        if self.field.attname in instance.__dict__:
+            del instance.__dict__[self.field.attname]
         
     def __get__(self, instance, owner=None):
         if instance is None:
             return self
         
         cache_name = self.field.get_cache_name()
-        if cache_name in instance.__dict__:
+        if cache_name in instance.__dict__ and isinstance(instance.__dict__[cache_name], datetime):
             return instance.__dict__[cache_name]
         
         if self.field.attname in instance.__dict__:
@@ -101,7 +100,7 @@ class TestDateTimeDescriptor(object):
             if not value is None:
                 if value.tzinfo is None:
                    value = db_tz.localize(value)
-                timezone = self.get_timezone(instance)
+                timezone = getattr(instance, self.field.get_timezone_name())
                 value = value.astimezone(timezone)
             instance.__dict__[cache_name] = value
             return value
@@ -109,7 +108,7 @@ class TestDateTimeDescriptor(object):
             value = instance.__dict__[self.field.name]
             if isinstance(value, datetime):
                 " В просто name попадают данные в текущем часовом поясе поля. "
-                timezone = self.get_timezone(instance)
+                timezone = getattr(instance, self.field.get_timezone_name())
                 if value.tzinfo is None:
                     value = timezone.localize(value)
                 else:
@@ -130,22 +129,64 @@ class TestDateTimeDescriptor(object):
             " В просто name попадают данные в текущем часовом поясе поля. "
             if not value is None:
                 if value.tzinfo is None:
-                    timezone = self.get_timezone(instance)
+                    timezone = getattr(instance, self.field.get_timezone_name())
                     value = timezone.localize(value)
                 value = value.astimezone(db_tz)
             return value
 
+def get_timezone(instance, timezone, cache_name):
+    """
+    Функция навешивается на класс модели, только если часовой пояс callable или lookup.
+    Если в кеше есть значение, у него есть timezone, значит можно взять его.
+    В кеше может лежать как datetime, так и tzinfo.
+    """
+    if cache_name in instance.__dict__:
+        value = instance.__dict__[cache_name]
+        if isinstance(value, datetime):
+            value = value.tzinfo
+        return value
+    
+    if callable(timezone):
+        " need call and converting "
+        timezone = timezone()
+    else:
+        " need lookup in db "
+        if not '__' in timezone:
+            timezone = getattr(instance, timezone)
+            if callable(timezone):
+                " supports both fields and methods "
+                timezone = timezone()
+        else:
+            timezone_parts = timezone.split('__')
+            while True:
+                part = '_%s_cache' % timezone_parts[0]
+                if part in instance.__dict__:
+                    instance = instance.__dict__[part]
+                    timezone_parts = timezone_parts[1:]
+                else:
+                    break
+            if len(timezone_parts) > 1:
+                timezone = instance._default_manager.filter(
+                    pk=instance._get_pk_val()
+                ).values_list('__'.join(timezone_parts))[0][0]
+            else:
+                timezone = getattr(instance, timezone_parts[0])
+            if timezone is None:
+                timezone = default_tz
+            print 'get_timezone: ', timezone_parts
+    
+    if isinstance(timezone, basestring):
+        timezone = pytz.timezone(timezone)
+    
+    instance.__dict__[cache_name] = timezone
+    return timezone 
+
 class TestDateTimeField(models.DateTimeField):
-    def __init__(self, verbose_name=None, name=None, timezone=None, cache=None, **kwargs):
+    def __init__(self, verbose_name=None, name=None, timezone=None, **kwargs):
         """
         timezone - Может быть объектом tytz.timezone, или callable, возвращающим tytz.timezone, или строкой. 
             Если timezone строка — то это либо название часового пояса, которое должно присутствовать 
             в pytz.all_timezones_set, либо лукап в базу данных.
-        cache — в случае, если timezone задан в виде лукапа в базу данных, он может генерироваться много запросов.
-            Для сохранения часового пояса можно использовать кеш. Это тпараметр задает время кеширования.
-            False — Не использовать кеш
-            True, None — Время кеширования по-умлочанию
-            Число — Время кеширования в секундах.
         """
         if isinstance(timezone, basestring):
             timezone = smart_str(timezone)
@@ -153,7 +194,6 @@ class TestDateTimeField(models.DateTimeField):
             self.timezone = pytz.timezone(timezone)
         else:
             self.timezone = timezone
-        self.cache = cache
         super(TestDateTimeField, self).__init__(verbose_name, name, **kwargs)
         
     def get_prep_value(self, value):
@@ -196,10 +236,6 @@ class TestDateTimeField(models.DateTimeField):
         " Значение было подготовленно выше, в pre_save "
         return self.get_db_prep_value(value, connection=connection, prepared=True)
     
-    def get_attname(self):
-        " Для хранения значения из базы в модели выбираем другое имя. "
-        return u'%s_utc' % self.name
-    
     def get_default(self):
         """
         Когда вызывается get_defaults, объекта нет, поэтому считаем часовой пояс приложения.
@@ -208,10 +244,42 @@ class TestDateTimeField(models.DateTimeField):
         value = super(TestDateTimeField, self).get_default()
         return self.get_prep_value(value)
     
+    def get_attname(self):
+        " Для хранения значения из базы в модели выбираем другое имя. "
+        return '%s_utc' % self.name
+    
+    def get_timezone_name(self):
+        return '%s_timezone' % self.name
+    
     def contribute_to_class(self, cls, name):
         super(TestDateTimeField, self).contribute_to_class(cls, name)
         setattr(cls, name, TestDateTimeDescriptor(self))
+        timezone = self.timezone
+        if timezone is None:
+            setattr(cls, self.get_timezone_name(), default_tz)
+        elif isinstance(timezone, tzinfo):
+            " already converted "
+            setattr(cls, self.get_timezone_name(), timezone)
+        else:
+            setattr(cls, self.get_timezone_name(), property(curry(get_timezone, timezone=timezone, cache_name=self.get_cache_name())))
 
+    def _get_val_from_obj(self, obj):
+        " Используется модулем сериализации. Почему-то для сериализвации берется attname, а для десериализации просто name. "
+        if obj is not None:
+            descriptor = getattr(type(obj), self.name)
+            return descriptor.__get__(model_instance)
+        else:
+            return self.get_default()
+    
+    def formfield(self, **kwargs):
+        """
+        Нужно запретить наследовать значение default, потому что для объекта оно должно быть в часовом поясе приложения,
+        а значения в формах должны быть в поясе объекта. Для ModelForm нужно передавать instance, с которого будет браться
+        актуальное значение. 
+        """
+        defaults = {'initial': None, 'show_hidden_initial': False}
+        defaults.update(kwargs)
+        return super(TestDateTimeField, self).formfield(**defaults)
 
 # allow South to handle TimeZoneField smoothly
 try:
